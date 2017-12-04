3b4ef30eb3a060e9580164821bd8ada219e1bfac
Krishnaiah, Jahnavi (Contractor) <jahnavidk261@gmail.com>
2017-12-04 10:32:25 -0500

    Initial Commit

---
+++ Mineswifter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mineswifter
+//
+//  Created by Krishnaiah, Jahnavi (Contractor) on 12/4/17.
+//  Copyright © 2017 Krishnaiah, Jahnavi (Contractor). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
